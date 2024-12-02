191bb18666d48028316bf8c221b0c7739b3da389
Fredrik Mile <fredrik.mile@gmail.com>
2024-12-02 07:07:42 +0100

    add day02.py

--- day02.py
+++ day02.py
@@ -14,7 +14,7 @@ def is_safe(report, part_two):
     levels = [int(level) for level in report.split()]
     safe = all_decreasing(levels) or all_increasing(levels)
     if not safe and part_two:
-        for i, v in enumerate(levels):
+        for i, _ in enumerate(levels):
             if all_decreasing(levels[:i] + levels[i+1:]) or all_increasing(levels[:i] + levels[i+1:]):
                 return True
 
